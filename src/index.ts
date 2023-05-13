492661ea4bc6ef0d3cc2a73e8bfb1a8916893fe0
David Prothero <david@prothero.com>
2023-05-13 14:04:56 -0700

    Add icon images

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
